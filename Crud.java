c5777edc05a57d66dcf7b00944e1cee1f40b7b4b
Alonso3s <136762821+Alonso3s@users.noreply.github.com>
2025-03-15 17:19:40 -0600

    Update Crud.java
    Añadí los métodos de menú y de modificarAlumnos

--- Crud.java
+++ Crud.java
@@ -15,13 +15,53 @@ public class Crud {
     ArrayList<Alumno> alum = new ArrayList<>();
     Alumno alumno = new Alumno();
     Scanner entrada = new Scanner(System.in);
+
+    public void menu(){
+                         int op=0;
+                 boolean validacion= false;
+                 while(!validacion){
+                   System.out.println("-------- MENÚ -------");  
+           System.out.println("Indique con un numero su acción");   
+                 System.out.println("1. Registar alumno"); 
+                 System.out.println("2. Mostrar alumnos registrados"); 
+                 System.out.println("3. Modificar dato/s de un alumno"); 
+                 System.out.println("4. Eliminra registro de alumno"); 
+                 System.out.println("5. Salir del programa");
+                 op=entrada.nextLine();
+                 if(op==1 || op==2 || op==3 || op==4 || op==5){
+                     validacion= true;
+                 }else{
+                   System.out.println("Entrada invalida");  
+                 }
+                 }
+        switch (op) { 
+            case 1:
+                 crearAlumno();
+             break;
+            case 2:
+                 mostrarAlumnos();
+             break;
+            case 3 :
+                 modificarAlumno();
+             break;
+             case 4 :
+                 eliminarAlumno();
+             break;
+             case 5 :
+                 exit(0);
+             break;
+            default:
+                 System.out.println("Entrada invalida"); 
+          }
+         }
+    }
     
     public void precargaAlumnos(){
     
-    alum.add(new Alumno("222", "jorge", "ie", "20", "f"));
-    alum.add(new Alumno("333", "alonso", "ie", "20", "f"));
-    alum.add(new Alumno("444", "michael", "ie", "20", "f"));
-    alum.add(new Alumno("111", "reiko", "ie", "20", "f"));
+    alum.add(new Alumno("222", "jorge", "ie", "21", "f"));
+    alum.add(new Alumno("333", "alonso", "ie", "21", "f"));
+    alum.add(new Alumno("444", "michael", "ie", "36", "f"));
+    alum.add(new Alumno("111", "reiko", "ie", "19", "f"));
     }
     
     public void crearAlumno(){
@@ -50,7 +90,7 @@ public class Crud {
             System.out.println(al);
         }
 }
-    public void encotrarAlumno(){
+   /* public void encotrarAlumno(){
         boolean encontrado = false;
     System.out.println("Ingrese el id a buscar");
     String id = entrada.nextLine();
@@ -66,11 +106,80 @@ public class Crud {
              
          }
         
-         }
+         }*/
         
  
     
     public void eliminarAlumnos(){
-
+    boolean encontrado = false;
+    System.out.println("Ingrese el id a buscar");
+    String id = entrada.nextLine();
+    for (Alumno al: alum) {
+         if (id.equals(al.getId())) {
+            encontrado= true;
+            Alumno.remove(alum);
+             System.out.println("Alumno eliminado con exito");
+            break;
+         }
+    }
+         if (!encontrado) {
+          System.out.println("Id no existe");
+             
+         }
+    }
+    public void modificarAlumno(){
+        boolean encontrado = false;
+        System.out.println("Ingrese el id a buscar");
+        String id = entrada.nextLine();
+        for (Alumno al: alum) {
+             if (id.equals(al.getId())) {
+            encontrado= true;
+                 int op=0;
+                 boolean validacion= false;
+                 while(!validacion){
+           System.out.println("Indique con un numero la opcion del dato que desea modificar");   
+                 System.out.println("1. Nombre"); 
+                 System.out.println("2. Carrera"); 
+                 System.out.println("3. Edad"); 
+                 System.out.println("4. Genero"); 
+                 System.out.println("5. Volver al menú");
+                 op=entrada.nextLine();
+                 if(op==1 || op==2 || op==3 || op==4 || op==5){
+                     validacion= true;
+                 }else{
+                   System.out.println("Entrada invalida");  
+                 }
+                 }
+        switch (op) { 
+            case 1:
+                 System.out.println("Ingrese el nombre del alumno: ");
+                 al.setName(entrada.nextLine());
+             break;
+            case 2:
+                 System.out.println("Ingrese la carrera del alumno: ");
+                 al.setCarrera(entrada.nextLine());
+             break;
+            case 3 :
+                 System.out.println("Ingrese la edad del alumno: ");
+                 al.setEdad(entrada.nextLine());
+             break;
+             case 4 :
+                 System.out.println("Ingrese el genero del alumno: ");
+                 al.setGenero(entrada.nextLine());
+             break;
+             case 5 :
+                 menu();
+             break;
+            default:
+                 System.out.println("Entrada invalida"); 
+          }
+             System.out.println("Alumno modificado con exito");
+            break;
+         }
+    }
+         if (!encontrado) {
+          System.out.println("Id no existe");
+             
+         }  
     }
 }
